01c285bd4877cd5e37cc9670559c4e4f8b7d7f29
menna <mennaayman200004@gmail.com>
2026-03-27 23:39:31 +0200

    fix

--- src/features/submitedApplication/submitedService.ts
+++ src/features/submitedApplication/submitedService.ts
@@ -4,7 +4,7 @@ import { PaginatedApplications } from "./submitedInterface";
 export const submitedService = {
   async getMyApplications(page = 0, size = 10): Promise<PaginatedApplications> {
     // استخدمي api مباشرة وشيلي <PaginatedApplications> من هنا لو لسه مدي لون أحمر
-    const response = await api.get("/api/applications/me", {
+    const response = await api.get("/applications/me", {
       params: { page, size },
     });
     
